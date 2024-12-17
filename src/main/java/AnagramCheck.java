3ffd3cab4e99100abb4851f6604c43e6c1ce093a
Chandrasheker <chandrashekermca@gmail.com>
2024-12-17 22:55:51 +0530

    Core java

---
+++ src/main/java/AnagramCheck.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+
+    public static void main(String[] args) {
+        System.out.println(new AnagramCheck().checkAnagram("ABC","CAB"));
+    }
+    public boolean checkAnagram(String s1, String s2){
+        boolean isAnagram=false;
+
+
+        char[] charArray = s1.toCharArray();
+        //ABC ABC
+
+        char[] charArray1 = s2.toCharArray();
+        Arrays.sort(charArray);
+        Arrays.sort(charArray1);
+        System.out.println(charArray);
+        System.out.println(charArray1);
+
+        System.out.println( Arrays.equals(charArray,charArray1));
+
+        if(Arrays.toString(charArray1).equals(Arrays.toString(charArray))){
+            isAnagram=true;
+        }
+
+
+
+        return  isAnagram;
+
+    }
+}
